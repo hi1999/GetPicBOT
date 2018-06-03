5fc32493929e50fa91e6c606a4a8be4d166299af
brandonpanty <39869446+brandonpanty@users.noreply.github.com>
2018-06-03 14:14:32 +0800

    Update Schedule_Report.js

--- Schedule_Report.js
+++ Schedule_Report.js
@@ -12,16 +12,10 @@ var bot = linebot({
     channelSecret: '0af678b6490a7452cd453250d9bd6998',
     channelAccessToken: 'IOHrMu7KzhjGMinqIDTpoXrxN/NlHFzF3ni7SYAAjN0SShmin7XW/5omFuSGq4tJuTvIdMstBWVpfADfORx7cT0/97Tkx5lmHY2z+77az/1hXYQfRCJGGUbOlSg15drJNNaojTkOSK3bU982zARsZQdB04t89/1O/w1cDnyilFU='
 });
-//Request IMGUR callback 隨機取圖
-var options = {
-    url: 'https://api.imgur.com/3/album/BJNxWqK/images',
-    headers: { 'Authorization': 'Client-ID d09fd3905abd246' }
-};
+
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
-        console.log("\t==>callback取圖OK: (" + info.data.length+")");
-
         const client = new Client({
             connectionString: process.env.DATABASE_URL,
             ssl: true,
