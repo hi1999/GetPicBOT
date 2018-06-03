7263c4b7b557bace8a9f9c1b83fdc90ac73d5212
brandonpanty <39869446+brandonpanty@users.noreply.github.com>
2018-06-03 09:06:40 +0000

    Update Schedule_Report.js

--- Schedule_Report.js
+++ Schedule_Report.js
@@ -80,13 +80,13 @@ function callback(error, response, body) {
         if (err) throw err;
             for (let row of res.rows) {
                 iFriend=row.count;
-                repMessage=     "            當日活躍人數:"+iGetUserToday+"人"+"\n"+
+                repMessage=     "           當日活躍人數:"+iGetUserToday+"人"+"\n"+
                                 "     當日抽的總次數:"+iSumToday+"次"+"\n"+
-                                "           抽的總次數:"+iSUM+"次"+"\n"+
-                                "       目前訂閱人數:"+iFriend+"人"+"\n"+
+                                "             抽的總次數:"+iSUM+"次"+"\n"+
+                                "        目前訂閱人數:"+iFriend+"人"+"\n"+
                                 "超過 30次抽的人數:"+iCOUNT30+"人"+"\n"+
                                 "超過100次抽的人數:"+iCOUNT100+"人"+"\n"+      
-                                "    活躍用戶比率為:"+iCOUNT30/iFriend*100+"%";
+                                "    活躍用戶比率為:"+iCOUNT30/iFriend*100+"%"+
                                 "    鐵粉比率為:"+iCOUNT100/iFriend*100+"%";
                 bot.push(ME, {
                     type: 'text',
