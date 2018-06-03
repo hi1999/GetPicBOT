830064924ce6c0ddf5bfb8272a9b2643b01bfaed
Sam Liu <hi1999@gmail.com>
2018-06-03 12:24:09 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -12,6 +12,7 @@ var bot = linebot({
 
 //測試heroku-postgresql
 const { Client } = require('pg');
+    console.log('process.env.DATABASE_URL=='+process.env.DATABASE_URL);
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
